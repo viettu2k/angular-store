5e6fc4739d31bcee50acc25a317f245529036740
viettu2k <doanviettu2k@gmail.com>
2023-01-11 22:08:58 +0700

    fix tailwind error and implementing header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -37,6 +38,7 @@ import { HeaderComponent } from './components/header/header.component';
     MatToolbarModule,
     MatBadgeModule,
     MatSnackBarModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  template: ` <p class="font-bold">header works!</p> `,
-  styles: [],
+  templateUrl: './header.component.html',
 })
 export class HeaderComponent implements OnInit {
   constructor() {}
