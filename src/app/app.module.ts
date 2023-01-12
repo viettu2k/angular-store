659c3617e71cb6d73b7454c8b10bd50b68e6edd3
viettu2k <doanviettu2k@gmail.com>
2023-01-12 22:10:45 +0700

    adding home page products-header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,9 +19,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './pages/home/home.component';
+import { ProductsHeaderComponent } from './pages/home/components/products-header/products-header.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent],
+  declarations: [AppComponent, HeaderComponent, HomeComponent, ProductsHeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
---
+++ src/app/pages/home/components/products-header/products-header.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products-header',
+  templateUrl: './products-header.component.html',
+})
+export class ProductsHeaderComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
