e6cf0a2e32cad7aa5dfc14f5936645bdfdb9cd04
viettu2k <doanviettu2k@gmail.com>
2023-01-15 21:09:01 +0700

    finish category button

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
   cols = 3;
+  category: string | undefined;
 
   constructor() {}
 
@@ -13,5 +14,9 @@ export class HomeComponent implements OnInit {
 
   onColumnsCountChange(colsNum: number): void {
     this.cols = colsNum;
+  }
+
+  onShowCategory(newCategory: string): void {
+    this.category = newCategory;
   }
 }
