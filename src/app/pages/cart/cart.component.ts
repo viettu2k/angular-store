e1bd294d82a890690eff3b17f63e9d7e63637f60
viettu2k <doanviettu2k@gmail.com>
2023-01-17 23:18:29 +0700

    adding cart page

---
+++ src/app/pages/cart/cart.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Cart, CartItem } from 'src/app/models/cart.model';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+})
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      {
+        product: 'https://via.placeholder.com/150',
+        name: 'shoes',
+        price: 150,
+        quantity: 65,
+        id: 1,
+      },
+    ],
+  };
+  dataSource: Array<CartItem> = [];
+  displayedColumns: Array<string> = [
+    'product',
+    'name',
+    'price',
+    'quantity',
+    'total',
+    'action',
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
+}
