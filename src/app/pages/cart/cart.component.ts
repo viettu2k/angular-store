8d283af5b4c2445cc1d3de2fc2756560c0c422e5
viettu2k <doanviettu2k@gmail.com>
2023-01-22 20:08:38 +0700

    implementing cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -13,7 +13,7 @@ export class CartComponent implements OnInit {
         product: 'https://via.placeholder.com/150',
         name: 'shoes',
         price: 150,
-        quantity: 67,
+        quantity: 43,
         id: 1,
       },
     ],
