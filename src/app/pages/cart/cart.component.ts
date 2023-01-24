31a37ba9269c5f6d8795210f302ce64650dbc8b6
viettu2k <doanviettu2k@gmail.com>
2023-01-24 22:21:02 +0700

    implementing cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -33,4 +33,10 @@ export class CartComponent implements OnInit {
   ngOnInit(): void {
     this.dataSource = this.cart.items;
   }
+
+  getTotal(items: Array<CartItem>): number {
+    return items
+      .map((item) => item.price * item.quantity)
+      .reduce((prev, current) => prev + current, 0);
+  }
 }
